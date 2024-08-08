074dc9709964b6ce07e46beb124329f57dfe80af
Rikhil2 <rikhiljkumar@gmail.com>
2024-08-07 23:10:51 -0400

    Correct tabnet pipeline

--- imputeEx.py
+++ imputeEx.py
@@ -2,57 +2,161 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 from gain import gain
+import tensorflow.compat.v1 as tf
+
+from utils import normalization, sample_batch_index, uniform_sampler, binary_sampler, rounding, renormalization, \
+    xavier_init
+
+tf.disable_v2_behavior()
 
 
-def main(args):
-    gain_parameters = {'batch_size': args.batch_size,
-                       'hint_rate': args.hint_rate,
-                       'alpha': args.alpha,
-                       'iterations': args.iterations}
+def train_imputer(X_train, gain_parameters):
+    miss_data_x = X_train.to_numpy()
+    data_m = 1 - np.isnan(miss_data_x)
 
-    # Load data
-    miss_data_x = np.loadtxt('data/' + 'converted' + '.csv', delimiter=",", skiprows=1)
+    batch_size = gain_parameters['batch_size']
+    hint_rate = gain_parameters['hint_rate']
+    alpha = gain_parameters['alpha']
+    iterations = gain_parameters['iterations']
 
-    # Impute missing data
-    imputed_data_x, MSE = gain(miss_data_x, gain_parameters)
+    no, dim = miss_data_x.shape
+    h_dim = int(dim)
 
-    if str(imputed_data_x[0][0]) != 'nan':
-        np.savetxt("data/adjusted/missing.csv", miss_data_x, delimiter=",")
-        np.savetxt("data/adjusted/imputed.csv", imputed_data_x, delimiter=",")
+    norm_data, norm_parameters = normalization(miss_data_x)
+    norm_data_x = np.nan_to_num(norm_data, 0)
 
-    return MSE
+    X = tf.placeholder(tf.float32, shape=[None, dim])
+    M = tf.placeholder(tf.float32, shape=[None, dim])
+    H = tf.placeholder(tf.float32, shape=[None, dim])
 
+    D_W1 = tf.Variable(xavier_init([dim * 2, h_dim]))
+    D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+    D_W2 = tf.Variable(xavier_init([h_dim, h_dim]))
+    D_b2 = tf.Variable(tf.zeros(shape=[h_dim]))
+    D_W3 = tf.Variable(xavier_init([h_dim, dim]))
+    D_b3 = tf.Variable(tf.zeros(shape=[dim]))
+    theta_D = [D_W1, D_W2, D_W3, D_b1, D_b2, D_b3]
 
-if __name__ == '__main__':
-    # Calls main function
+    G_W1 = tf.Variable(xavier_init([dim * 2, h_dim]))
+    G_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+    G_W2 = tf.Variable(xavier_init([h_dim, h_dim]))
+    G_b2 = tf.Variable(tf.zeros(shape=[h_dim]))
+    G_W3 = tf.Variable(xavier_init([h_dim, dim]))
+    G_b3 = tf.Variable(tf.zeros(shape=[dim]))
+    theta_G = [G_W1, G_W2, G_W3, G_b1, G_b2, G_b3]
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--batch_size',
-        help='the number of samples in mini-batch',
-        default=128,
-        type=int)
-    parser.add_argument(
-        '--hint_rate',
-        help='hint probability',
-        default=0.9,
-        type=float)
-    parser.add_argument(
-        '--alpha',
-        help='hyperparameter',
-        default=1e4,
-        type=float)
-    parser.add_argument(
-        '--iterations',
-        help='number of training iterations',
-        default=10000,
-        type=int)
-    args = parser.parse_args()
-    mse = main(args)
-    while str(mse) == 'nan':
-        mse = main(args)
-    print(mse)
+    def generator(x, m):
+        inputs = tf.concat(values=[x, m], axis=1)
+        G_h1 = tf.nn.relu(tf.matmul(inputs, G_W1) + G_b1)
+        G_h2 = tf.nn.relu(tf.matmul(G_h1, G_W2) + G_b2)
+        G_prob = tf.nn.sigmoid(tf.matmul(G_h2, G_W3) + G_b3)
+        return G_prob
+
+    def discriminator(x, h):
+        inputs = tf.concat(values=[x, h], axis=1)
+        D_h1 = tf.nn.relu(tf.matmul(inputs, D_W1) + D_b1)
+        D_h2 = tf.nn.relu(tf.matmul(D_h1, D_W2) + D_b2)
+        D_logit = tf.matmul(D_h2, D_W3) + D_b3
+        D_prob = tf.nn.sigmoid(D_logit)
+        return D_prob
+
+    G_sample = generator(X, M)
+    Hat_X = X * M + G_sample * (1 - M)
+    D_prob = discriminator(Hat_X, H)
+
+    D_loss_temp = -tf.reduce_mean(M * tf.log(D_prob + 1e-8) + (1 - M) * tf.log(1. - D_prob + 1e-8))
+    G_loss_temp = -tf.reduce_mean((1 - M) * tf.log(D_prob + 1e-8))
+    MSE_loss = tf.reduce_mean((M * X - M * G_sample) ** 2) / tf.reduce_mean(M)
+
+    D_loss = D_loss_temp
+    G_loss = G_loss_temp + alpha * MSE_loss
+
+    D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
+    G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    best_loss = float('inf')
+    patience_counter = 0
+
+    for it in range(iterations):
+        batch_idx = sample_batch_index(no, batch_size)
+        X_mb = norm_data_x[batch_idx, :]
+        M_mb = data_m[batch_idx, :]
+        Z_mb = uniform_sampler(0, 0.01, batch_size, dim)
+        H_mb_temp = binary_sampler(hint_rate, batch_size, dim)
+        H_mb = M_mb * H_mb_temp
+        X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
+
+        _, D_loss_curr = sess.run([D_solver, D_loss_temp], feed_dict={M: M_mb, X: X_mb, H: H_mb})
+        _, G_loss_curr, MSE_loss_curr = sess.run([G_solver, G_loss_temp, MSE_loss], feed_dict={X: X_mb, M: M_mb, H: H_mb})
+
+        if MSE_loss_curr < best_loss:
+            best_loss = MSE_loss_curr
+            patience_counter = 0
+        else:
+            patience_counter += 1
+            if patience_counter >= 5:
+                print(f"Early stopping on iteration {it}. Best loss: {best_loss}")
+                break
+
+    Z_mb = uniform_sampler(0, 0.01, no, dim)
+    M_mb = data_m
+    X_mb = norm_data_x
+    X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
+
+    imputed_data = sess.run([G_sample], feed_dict={X: X_mb, M: M_mb})[0]
+
+    imputed_data = data_m * norm_data_x + (1 - data_m) * imputed_data
+
+    # Renormalization
+    imputed_data = renormalization(imputed_data, norm_parameters)
+
+    # Rounding
+    imputed_data = rounding(imputed_data, miss_data_x)
+
+    X_train = pd.DataFrame(imputed_data, columns=X_test.columns)
+    return sess, G_sample, X, M, norm_parameters, X_train
+
+def impute_test_data(sess, G_sample, X, M, norm_parameters, X_test):
+    miss_data_x = X_test.to_numpy()
+    data_m = 1 - np.isnan(miss_data_x)
+    no, dim = miss_data_x.shape
+    norm_data, _ = normalization(miss_data_x)
+    norm_data_x = np.nan_to_num(norm_data, 0)
+
+    Z_mb = uniform_sampler(0, 0.01, no, dim)
+    M_mb = data_m
+    X_mb = norm_data_x
+    X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
+
+    imputed_data = sess.run([G_sample], feed_dict={X: X_mb, M: M_mb})[0]
+    imputed_data = data_m * norm_data_x + (1 - data_m) * imputed_data
+    imputed_data = renormalization(imputed_data, norm_parameters)
+    imputed_data = rounding(imputed_data, miss_data_x)
+
+    return pd.DataFrame(imputed_data, columns=X_test.columns)
+
+# Usage
+df = pd.read_csv('data/converted.csv')
+X = df.drop(['Quit_FU'], axis=1)
+y = df['Quit_FU']
+X = X.fillna(np.nan)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+gain_parameters = {'batch_size': 128, 'hint_rate': 0.9, 'alpha': 1e4, 'iterations': 10000}
+
+# Train the imputer
+sess, G_sample, X, M, norm_parameters, X_train_imputed = train_imputer(X_train, gain_parameters)
+
+# Impute the test data
+X_test_imputed = impute_test_data(sess, G_sample, X, M, norm_parameters, X_test)
+X_train_imputed.to_csv('X_train_imputed.csv', index=False)
+X_test_imputed.to_csv('X_test_imputed.csv', index=False)
--- smoking_tabnet.py
+++ smoking_tabnet.py
@@ -4,84 +4,190 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin
+from hyperopt import fmin, Trials, STATUS_OK, tpe, hp
 from pytorch_tabnet.tab_model import TabNetClassifier
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import make_scorer, mean_squared_error, accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from tqdm import tqdm
 
 from gain import gain
-from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+import tensorflow.compat.v1 as tf
+
+from utils import normalization, sample_batch_index, uniform_sampler, binary_sampler, rounding, renormalization, \
+    xavier_init
+
+tf.disable_v2_behavior()
+
+def train_imputer(X_train, gain_parameters):
+    miss_data_x = X_train.to_numpy()
+    data_m = 1 - np.isnan(miss_data_x)
+
+    batch_size = gain_parameters['batch_size']
+    hint_rate = gain_parameters['hint_rate']
+    alpha = gain_parameters['alpha']
+    iterations = gain_parameters['iterations']
+
+    no, dim = miss_data_x.shape
+    h_dim = int(dim)
+
+    norm_data, norm_parameters = normalization(miss_data_x)
+    norm_data_x = np.nan_to_num(norm_data, 0)
+
+    X = tf.placeholder(tf.float32, shape=[None, dim])
+    M = tf.placeholder(tf.float32, shape=[None, dim])
+    H = tf.placeholder(tf.float32, shape=[None, dim])
+
+    D_W1 = tf.Variable(xavier_init([dim * 2, h_dim]))
+    D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+    D_W2 = tf.Variable(xavier_init([h_dim, h_dim]))
+    D_b2 = tf.Variable(tf.zeros(shape=[h_dim]))
+    D_W3 = tf.Variable(xavier_init([h_dim, dim]))
+    D_b3 = tf.Variable(tf.zeros(shape=[dim]))
+    theta_D = [D_W1, D_W2, D_W3, D_b1, D_b2, D_b3]
+
+    G_W1 = tf.Variable(xavier_init([dim * 2, h_dim]))
+    G_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+    G_W2 = tf.Variable(xavier_init([h_dim, h_dim]))
+    G_b2 = tf.Variable(tf.zeros(shape=[h_dim]))
+    G_W3 = tf.Variable(xavier_init([h_dim, dim]))
+    G_b3 = tf.Variable(tf.zeros(shape=[dim]))
+    theta_G = [G_W1, G_W2, G_W3, G_b1, G_b2, G_b3]
+
+    def generator(x, m):
+        inputs = tf.concat(values=[x, m], axis=1)
+        G_h1 = tf.nn.relu(tf.matmul(inputs, G_W1) + G_b1)
+        G_h2 = tf.nn.relu(tf.matmul(G_h1, G_W2) + G_b2)
+        G_prob = tf.nn.sigmoid(tf.matmul(G_h2, G_W3) + G_b3)
+        return G_prob
+
+    def discriminator(x, h):
+        inputs = tf.concat(values=[x, h], axis=1)
+        D_h1 = tf.nn.relu(tf.matmul(inputs, D_W1) + D_b1)
+        D_h2 = tf.nn.relu(tf.matmul(D_h1, D_W2) + D_b2)
+        D_logit = tf.matmul(D_h2, D_W3) + D_b3
+        D_prob = tf.nn.sigmoid(D_logit)
+        return D_prob
+
+    G_sample = generator(X, M)
+    Hat_X = X * M + G_sample * (1 - M)
+    D_prob = discriminator(Hat_X, H)
+
+    D_loss_temp = -tf.reduce_mean(M * tf.log(D_prob + 1e-8) + (1 - M) * tf.log(1. - D_prob + 1e-8))
+    G_loss_temp = -tf.reduce_mean((1 - M) * tf.log(D_prob + 1e-8))
+    MSE_loss = tf.reduce_mean((M * X - M * G_sample) ** 2) / tf.reduce_mean(M)
+
+    D_loss = D_loss_temp
+    G_loss = G_loss_temp + alpha * MSE_loss
+
+    D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
+    G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    best_loss = float('inf')
+    patience_counter = 0
+
+    for it in range(iterations):
+        batch_idx = sample_batch_index(no, batch_size)
+        X_mb = norm_data_x[batch_idx, :]
+        M_mb = data_m[batch_idx, :]
+        Z_mb = uniform_sampler(0, 0.01, batch_size, dim)
+        H_mb_temp = binary_sampler(hint_rate, batch_size, dim)
+        H_mb = M_mb * H_mb_temp
+        X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
+
+        _, D_loss_curr = sess.run([D_solver, D_loss_temp], feed_dict={M: M_mb, X: X_mb, H: H_mb})
+        _, G_loss_curr, MSE_loss_curr = sess.run([G_solver, G_loss_temp, MSE_loss],
+                                                 feed_dict={X: X_mb, M: M_mb, H: H_mb})
+
+        if MSE_loss_curr < best_loss:
+            best_loss = MSE_loss_curr
+            patience_counter = 0
+        else:
+            patience_counter += 1
+            if patience_counter >= 5:
+                print(f"Early stopping on iteration {it}. Best loss: {best_loss}")
+                break
+
+    Z_mb = uniform_sampler(0, 0.01, no, dim)
+    M_mb = data_m
+    X_mb = norm_data_x
+    X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
+
+    imputed_data = sess.run([G_sample], feed_dict={X: X_mb, M: M_mb})[0]
+
+    imputed_data = data_m * norm_data_x + (1 - data_m) * imputed_data
+
+    # Renormalization
+    imputed_data = renormalization(imputed_data, norm_parameters)
+
+    # Rounding
+    imputed_data = rounding(imputed_data, miss_data_x)
+
+    X_train = pd.DataFrame(imputed_data, columns=X_train.columns)
+    return sess, G_sample, X, M, norm_parameters, X_train
 
 
-def imputer(data_path):
-    gain_parameters = {'batch_size': 128,
-                       'hint_rate': 0.9,
-                       'alpha': 1e4,
-                       'iterations': 10000}
+def impute_test_data(sess, G_sample, X, M, norm_parameters, X_test):
+    miss_data_x = X_test.to_numpy()
+    data_m = 1 - np.isnan(miss_data_x)
+    no, dim = miss_data_x.shape
+    norm_data, _ = normalization(miss_data_x)
+    norm_data_x = np.nan_to_num(norm_data, 0)
 
-    # Load data
-    miss_data_x = np.loadtxt(data_path, delimiter=",", skiprows=1)
+    Z_mb = uniform_sampler(0, 0.01, no, dim)
+    M_mb = data_m
+    X_mb = norm_data_x
+    X_mb = M_mb * X_mb + (1 - M_mb) * Z_mb
 
-    # Impute missing data
-    imputed_data_x, MSE = gain(miss_data_x, gain_parameters)
+    imputed_data = sess.run([G_sample], feed_dict={X: X_mb, M: M_mb})[0]
+    imputed_data = data_m * norm_data_x + (1 - data_m) * imputed_data
+    imputed_data = renormalization(imputed_data, norm_parameters)
+    imputed_data = rounding(imputed_data, miss_data_x)
 
-    while str(imputed_data_x[0][0]) == 'nan':
-        imputed_data_x, MSE = gain(miss_data_x, gain_parameters)
-
-    np.savetxt("data/temp.csv", imputed_data_x, delimiter=",")
-
-    return MSE
-
-
-def imputeFunction(data):
-    data.to_csv('data/temp.csv', index=False)
-    # np.savetxt("data/temp1.csv", data, delimiter=",")
-    MSE = imputer('data/temp.csv')
-    print(MSE)
-    data = pd.read_csv('data/temp.csv',
-                       names=['GENDER_SRC_DESC', 'MARITAL_STATUS_AT_DX_DESC', 'ETHNICITY_SRC_DESC',
-                              'RACE_CR_SRC_DESC_1', 'HISTOLOGY_CD', 'PRIMARY_SITE_CD',
-                              'PRIMARY_SITE_GROUP_DESC', 'PRIMARY_SITE_REGION_DESC',
-                              'METS_AT_DX_DISTANT_LYMPH_NODES_DESC', 'METS_AT_DX_OTHER_DESC',
-                              'SUMMARY_OF_RX_1ST_COURSE_DESC', 'SURGERY_RADIATION_SEQ_NUM',
-                              'SYSTEMIC_RX_SURGERY_SEQ_NUM', 'DIAGNOSTIC_CONFIRMATION_METHOD_DESC',
-                              'TUMOR_BEHAVIOR_DESC', 'VITAL_STATUS', 'Cessation Meds',
-                              'Cessation Meds Past 90 Days', 'CRA_SMOKE_5PK_EVER',
-                              'CRA_SMOKE_HOME_EXPOSED', 'CRA_SMOKE_WORK_EXPOSED', 'CRA_TOBACCO_SNUFF',
-                              'AGE_AT_DIAGNOSIS_NUM', 'REGIONAL_NODES_EXAMINED',
-                              'REGIONAL_NODES_POSITIVE', 'SURVIVAL_TIME_IN_MONTHS',
-                              'PreTreatment_Smoke_Status', 'Tumor_size', 'Quit_Status_Any_time',
-                              'CRA_SMOKE_AGE', 'CRA_SMOKE_TOTALYRS']
-                       )
-    print(data.shape)
-    data = data.astype('int64')
-    return data
+    return pd.DataFrame(imputed_data, columns=X_test.columns)
 
 
 class CustomImputer(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+    def __init__(self, gain_parameters):
+        self.gain_parameters = gain_parameters
+        self.sess = None
+        self.G_sample = None
+        self.Q = None
+        self.M = None
+        self.norm_parameters = None
 
     def fit(self, X, y=None):
+        self.sess, self.G_sample, self.Q, self.M, self.norm_parameters, _ = train_imputer(X, self.gain_parameters)
         return self
 
     def transform(self, X):
         X_copy = X.copy()
-        X_transformed = imputeFunction(X_copy)
-        return X_transformed
+        if X_copy.shape[0] < 4000:
+            _, _, _, _, _, X_train_imputed = train_imputer(X_copy, self.gain_parameters)
+            return X_train_imputed
+        else:
+            X_test_imputed = impute_test_data(self.sess, self.G_sample, self.Q, self.M, self.norm_parameters, X_copy)
+            return X_test_imputed
 
 
+# Load the data
 df = pd.read_csv('data/converted.csv')
 X = df.drop(['Quit_FU'], axis=1)
 y = df['Quit_FU']
-cols = list(X.columns)
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
-X_train = X_train.fillna('nan')
-X_test = X_test.fillna('nan')
-# X_adj = imputeFunction(X_train)
-# X_adj.to_csv('imputed.csv', index=False)
+X = X.fillna(np.nan)
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+gain_parameters = {'batch_size': 128, 'hint_rate': 0.9, 'alpha': 1e4, 'iterations': 10000}
+
+# Train the imputer
+# sess, G_sample, X, M, norm_parameters, X_train_imputed = train_imputer(X_train_val, gain_parameters)
+# X_test_imputed = impute_test_data(sess, G_sample, X, M, norm_parameters, X_test)
+
 
 space = {
     'n_d': hp.quniform('n_d', 8, 64, 1),
@@ -101,13 +207,14 @@ def objective(params):
     params['n_independent'] = int(params['n_independent'])
     params['n_shared'] = int(params['n_shared'])
     pipeline = Pipeline([
-        ("imputer", CustomImputer()),
+        ("imputer", CustomImputer(gain_parameters)),
         ("scaler", StandardScaler()),
         ("poly", PolynomialFeatures(degree=2)),
         ("classifier", TabNetClassifier(**params))
     ])
     cv = StratifiedKFold(n_splits=5, shuffle=True)
-    scores = cross_val_score(pipeline, X_train, y_train.values, cv=cv, scoring='neg_log_loss', n_jobs=-1)
+    scorer = make_scorer(mean_squared_error, greater_is_better=False)
+    scores = cross_val_score(pipeline, X_train_val, y_train_val, cv=cv, scoring=scorer, n_jobs=-1)
     avg_score = np.mean(scores)
     return {'loss': avg_score, 'status': STATUS_OK}
 
@@ -115,25 +222,25 @@ def objective(params):
 trials = Trials()
 
 best_params = fmin(objective, space, algo=tpe.suggest, max_evals=10, trials=trials)
-# best_params['n_d'] = int(best_params['n_d'])
-# best_params['n_a'] = int(best_params['n_a'])
-# best_params['n_steps'] = int(best_params['n_steps'])
-# best_params['n_independent'] = int(best_params['n_independent'])
-# best_params['n_shared'] = int(best_params['n_shared'])
-# print("Best set of parameters: {}".format(best_params))
-#
-# pipe = Pipeline([
-#     ("imputer", Transformer(imputeFunction)),
-#     ("scaler", StandardScaler()),
-#     ("poly", PolynomialFeatures(degree=2)),
-#     ("classifier", TabNetClassifier(**best_params))
-# ])
-#
-# pipe.fit(X_train.values, y_train.values)
-# predictions = pipe.predict(X_test.values)
-# print(y_test.value_counts())
-# print(accuracy_score(y_test, predictions))
-# print(roc_auc_score(y_test, predictions))
-# # accuracy:
-# # roc_auc:
-# # majority class percentage:
+best_params['n_d'] = int(best_params['n_d'])
+best_params['n_a'] = int(best_params['n_a'])
+best_params['n_steps'] = int(best_params['n_steps'])
+best_params['n_independent'] = int(best_params['n_independent'])
+best_params['n_shared'] = int(best_params['n_shared'])
+print("Best set of parameters: {}".format(best_params))
+
+pipe = Pipeline([
+    ("imputer", CustomImputer()),
+    ("scaler", StandardScaler()),
+    ("poly", PolynomialFeatures(degree=2)),
+    ("classifier", TabNetClassifier(**best_params))
+])
+
+pipe.fit(X_train_val.values, y_train_val.values)
+predictions = pipe.predict(X_test.values)
+print(y_test.value_counts())
+print(accuracy_score(y_test, predictions))
+print(roc_auc_score(y_test, predictions))
+# accuracy:
+# roc_auc:
+# majority class percentage:
